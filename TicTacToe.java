29d9c877e4925c179f22d43b54e90e4a70129884
Vanshika-Dhamija <115693210+Vanshika-Dhamija@users.noreply.github.com>
2023-03-31 17:10:13 +0530

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -4,9 +4,6 @@ public class TicTacToe {
     public static void main(String args[]) {
 
         Scanner scan = new Scanner(System.in);
-
-        System.out.println("\n***** TIC TAC TOE *****");
-
         System.out.println("\nEnter 1 For MultiPlayer");
         System.out.println("Enter 2 To Play With Computer");
         System.out.println("Enter 0 To Quit\n");
